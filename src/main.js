5757190c985754cab61f0adc392acef8068ec098
lx-ghb <3524317373@qq.com>
2019-09-18 18:40:28 +0800

    推送src

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' //  引入element-ui
+import 'element-ui/lib/theme-chalk/index.css' //  引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) //  注册elementui
 new Vue({
   router,
   render: h => h(App)
